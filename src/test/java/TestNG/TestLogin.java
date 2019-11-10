33fb056e18259109112aa7f505de82af1fc1c1cd
prabathmk <57253495+prabathmk@users.noreply.github.com>
2019-11-10 14:36:01 +0530

    Modified File
    Modified the TestLogin.java

--- src/test/java/TestNG/TestLogin.java
+++ src/test/java/TestNG/TestLogin.java
@@ -1,9 +1,12 @@
 package TestNG;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestLogin {
@@ -11,23 +14,29 @@ public class TestLogin {
 	
 	static WebDriver ffdriver;
 	
+	@BeforeMethod
+	public static void launch() {
+		
+		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+		ffdriver.get("http://localhost:3001/");
+		ffdriver.manage().window().maximize();
+		ffdriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	
+	}
+	
 	@Test
 	public static void testLogin() {
 		
-	System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
-	ffdriver.get("http://localhost:3001/");
-	ffdriver.manage().window().maximize();
-	
-	Assert.assertEquals( ffdriver.findElement(By.xpath("/html/body/form/div/div/div/div[1]/div[2]")).getText(),"devops");
-
-	Assert.assertEquals( ffdriver.findElement(By.xpath("/html/body/form/div/div/div/div[1]/div[4]")).getText(),"test");
+		
+	ffdriver.findElement(By.xpath("/html/body/form/div/div/div/div[1]/div[2]")).sendKeys("devops");
+	ffdriver.findElement(By.xpath("/html/body/form/div/div/div/div[1]/div[4]")).sendKeys("test");
+	ffdriver.findElement(By.xpath("/html/body/form/div/div/div/div[1]/div[6]")).click();
 	
 	}
 	
 	@AfterMethod
 	public static void end()
 	{
-		ffdriver.quit();
+		ffdriver.close();
 	}
 	
 
